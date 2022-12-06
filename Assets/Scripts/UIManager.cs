43377de99070a32944bb40455b782adbcdbbbf45
Connor Alan Kaupert <conkaup2@gmail.com>
2022-12-05 21:19:32 -0500

    Finishing Touches

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -11,8 +11,6 @@ public class UIManager : MonoBehaviour
     public TextMeshProUGUI count;
     public TextMeshProUGUI govText;
 
-    public TextMeshProUGUI quotaTest;
-
     public CanvasGroup timerParent;
     public CanvasGroup GovernmentParent;
 
@@ -22,7 +20,6 @@ public class UIManager : MonoBehaviour
     private bool nextText = false;
 
     private bool secondChance = true, gameOver = false;
-    private int govFood;
 
     public int govQuota;
 
@@ -37,13 +34,6 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F) && FarmManager._instance.GetCrops() > 0)
-        {
-            FarmManager._instance.TakeCrops();
-            govFood++;
-        }
-        quotaTest.text = (govFood).ToString();
-
         if(!nextText)
         {
             if (DayTimer >= 0f)
@@ -55,21 +45,21 @@ public class UIManager : MonoBehaviour
             {
                 if (dayCount != 0)
                 {
-                    DayTimer = 24f;
+                    DayTimer = 30f;
                     timer.text = ((int)DayTimer).ToString();
                     dayCount--;
                     count.text = dayCount.ToString();
                 }
                 else if (dayCount == 0)
                 {
-                    if (govFood >= govQuota)
+                    if (FarmManager._instance.GetCrops() >= govQuota)
                     {
                         nextText = true;
                         timerParent.alpha = 0f;
                         GovernmentParent.alpha = 1f;
                         govText.text = "Great job comrade!\n\nYour harvest will serve the union well!";
 
-                        DayTimer = 24f;
+                        DayTimer = 30f;
                         timer.text = ((int)DayTimer).ToString();
                         dayCount = 7;
                         count.text = dayCount.ToString();
@@ -82,14 +72,14 @@ public class UIManager : MonoBehaviour
                         govText.text = "comrade?\n\nDo you hate the motherland?\nYou must as you didn't do your part to sustain her! Soldiers arrest this traitor!";
                         gameOver = true;
                     }
-                    else if (govFood < govQuota)
+                    else if (FarmManager._instance.GetCrops() < govQuota)
                     {
                         nextText = true;
                         timerParent.alpha = 0f;
                         GovernmentParent.alpha = 1f;
                         govText.text = "COMRADE!\n\nYour have failed your motherland!\nFortunately for you she is forgiving.\nDO NOT FAIL HER AGAIN!";
 
-                        DayTimer = 24f;
+                        DayTimer = 30f;
                         timer.text = ((int)DayTimer).ToString();
                         dayCount = 7;
                         count.text = dayCount.ToString();
@@ -112,7 +102,7 @@ public class UIManager : MonoBehaviour
                     nextText = false;
                     timerParent.alpha = 1f;
                     GovernmentParent.alpha = 0f;
-                    govFood = 0;
+                    FarmManager._instance.ResetCrop();
                 }
             }
         }
